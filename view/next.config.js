d4b2944526ac0704bf3dd2aedd3380c8676bc895
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-11-04 19:17:15 +0700

    config

--- view/next.config.js
+++ view/next.config.js
@@ -1,7 +1,17 @@
-const withCSS = require('@zeit/next-css')
-const withTM = require('next-transpile-modules')
+const withSass = require('@zeit/next-sass')
+const withCSS = require("@zeit/next-css")
+module.exports = withCSS(withSass({
+  webpack (config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000
+        }
+      }
+    })
 
-module.exports = withCSS(withTM({
-  transpileModules: ['next-mui-helper'],
-  distDir: '../.next',
+    return config
+  }
 }))
