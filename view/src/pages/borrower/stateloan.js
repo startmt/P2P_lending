b8e95d3bdf3fcc5f544d54a0b09116167b703428
blvcklvgxxn <qant_elite@hotmail.com>
2019-11-29 03:38:36 +0700

    Page loancondition and stateloan

---
+++ view/src/pages/borrower/stateloan.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react'
+import { compose, bindActionCreators } from 'redux'
+import { pageNameAction } from '~/modules/query/actions'
+import withRedux from '~/hocs/with-redux'
+import { DashboardLayout } from '~/layouts/dashboard'
+import StateLoaner from '~/modules/borrower/containers/StateLoaner/StateLoaner'
+const Index = (props) => {
+  const { setPageName } = props
+  setPageName('stateLoan')
+  return (
+    <DashboardLayout>
+      <StateLoaner />
+    </DashboardLayout>
+  )
+}
+const mapStateToProps = null
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      setPageName: pageNameAction.setPageName,
+    },
+    dispatch,
+  )
+export default compose(
+  withRedux(mapStateToProps, mapDispatchToProps),
+)(Index)
