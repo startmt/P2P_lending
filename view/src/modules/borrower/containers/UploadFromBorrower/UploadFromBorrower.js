a51787790d2f1a5dbb44de880b59db309c7ef398
blvcklvgxxn <qant_elite@hotmail.com>
2019-11-29 19:37:39 +0700

    UploadFromBorrower Complete

---
+++ view/src/modules/borrower/containers/UploadFromBorrower/UploadFromBorrower.js
@@ -0,0 +1,154 @@
+import { Upload, Icon, message } from 'antd'
+import './style.less'
+
+const { Dragger } = Upload
+
+const props = {
+  name: 'file',
+  multiple: true,
+  action:
+    'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList)
+    }
+    if (status === 'done') {
+      message.success(
+        `${info.file.name} file uploaded successfully.`,
+      )
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`)
+    }
+  },
+}
+
+const UploadFromBorrow = () => {
+  return (
+    <div className="fromContainer">
+      <h3>
+        <div className="topicFrom">อัพโหลดเอกสาร</div>
+      </h3>
+      <div className="uploadTopicFrom">
+        กรุณาตรวจสอบเอกสารของคุณให้ถูกต้องและครบถ้วนก่อนอัพโหลดเอกสารเพื่อความรวดเร็วในการอณุมัติเอกสาร
+      </div>
+      <br />
+      <div>
+        <div className="adviceTopic">
+          *ข้อแนะนำในการอัพโหลดเอกสาร
+        </div>
+        <div className="advice">
+          1.สแกนหนึ่งเอกสารต่อหนึ่งไฟล์
+          <br />
+          2.เรียงลำดับหน้าถูกต้อง
+          <br />
+          3.เอกสารปรากฏชัดเจนทั้งหน้า
+          <br />
+          4.หากอัพโหลดหน้าละไฟล์ ควรตั้งชื่อไฟล์เรียงกัน
+          <br />
+          5.ขนาดไฟล์ไม่เกิน 20 MB
+          <br />
+          6.นามสกุลไฟล์ของเอกสารควรเป็น jpg pdf และ png
+          เท่านั้น
+        </div>
+      </div>
+      <br />
+      <div className="uploadFileContainer">
+        <div className="uploadForm">
+          <div className="uploadTopic">
+            1. หนังสือรับรองการจดทะเบียนบริษัทอายุไม่เกิน 3
+            เดือน
+          </div>
+          <br />
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="inbox" />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+          </Dragger>
+        </div>
+        <br />
+        <div className="uploadForm">
+          <div className="uploadTopic">
+            2. บัญชีธนาคารหลักแสดงรายการ 6
+            เดือนย้อนหลังของบริษัท
+          </div>
+          <br />
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="inbox" />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+          </Dragger>
+        </div>
+        <br />
+        <div className="uploadForm">
+          <div className="uploadTopic">
+            3. รานงานเครดิตบูโรของบริษัท
+            [ข้อมูลล่าสุดไม่เกิน 30 วัน]
+          </div>
+          <br />
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="inbox" />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+          </Dragger>
+        </div>
+        <br />
+        <div className="uploadForm">
+          <div className="uploadTopic">
+            4. งบการเงินที่สอบทานล่าสุด
+          </div>
+          <br />
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="inbox" />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+          </Dragger>
+        </div>
+        <br />
+        <div className="uploadForm">
+          <div className="uploadTopic">
+            5. ใบทะเบียนพาณิชย์
+          </div>
+          <br />
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="inbox" />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+          </Dragger>
+        </div>
+        <br />
+        <div className="uploadForm">
+          <div className="uploadTopic">
+            6. บัญชีรายชื่อผู้ถือหุ้น บอจ. 5
+          </div>
+          <br />
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="inbox" />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+          </Dragger>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UploadFromBorrow
--- view/src/pages/borrower/uploadborrowerfrom.js
+++ view/src/pages/borrower/uploadborrowerfrom.js
@@ -1,11 +1,26 @@
-เระ import React, { Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { compose, bindActionCreators } from 'redux'
 import { pageNameAction } from '~/modules/query/actions'
 import withRedux from '~/hocs/with-redux'
 import { DashboardLayout } from '~/layouts/dashboard'
-import UploadFromBorrower from '~/modules/borrower/containers/UploadFromBorrower'
-<div id="container" style="padding: 24px"></div>
-<script>
-  var mountNode = document.getElementById('container');
-
-</script>
+import UploadFromBorrower from '~/modules/borrower/containers/UploadFromBorrower/UploadFromBorrower'
+const Index = (props) => {
+  const { setPageName } = props
+  setPageName('uploadFromBorrower')
+  return (
+    <DashboardLayout>
+      <UploadFromBorrower />
+    </DashboardLayout>
+  )
+}
+const mapStateToProps = null
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      setPageName: pageNameAction.setPageName,
+    },
+    dispatch,
+  )
+export default compose(
+  withRedux(mapStateToProps, mapDispatchToProps),
+)(Index)
