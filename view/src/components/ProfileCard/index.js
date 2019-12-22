bb2a129a4c8bc70adafb252138e6593473e359a6
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-12-22 19:11:38 +0700

    hotfix frontend profile

--- view/src/components/ProfileCard/index.js
+++ view/src/components/ProfileCard/index.js
@@ -1,26 +1,41 @@
 import React from 'react'
 import { Card, Descriptions } from 'antd'
-const ProfileCard = () => (
-  <Card>
-    <Descriptions title="ข้อมูลส่วนตัว">
-      <Descriptions.Item label="ชื่อ-นามสกุล">
-        ชาญศิลป์ ทองคำ
-      </Descriptions.Item>
-      <Descriptions.Item label="เบอร์โทรศัพท์">
-        0910110111
-      </Descriptions.Item>
-      <Descriptions.Item label="วันเกิด">
-        02/09/1997
-      </Descriptions.Item>
-      <Descriptions.Item label="เลขบัตรประจำตัวประชาชน">
-        1200101730094
-      </Descriptions.Item>
-      <Descriptions.Item label="ที่อยู่">
-        No. 18, Wantang Road, Xihu District, Hangzhou,
-        Zhejiang, China
-      </Descriptions.Item>
-    </Descriptions>
-  </Card>
-)
+import { useSelector } from 'react-redux'
+const ProfileCard = () => {
+  const authDetailSelector = useSelector((state) =>
+    state.getIn(['authentication', 'auth', 'userDetail']),
+  )
+  const addressJson = JSON.parse(
+    authDetailSelector.get('address') || '{}',
+  )
+  return (
+    <Card>
+      <Descriptions title="ข้อมูลส่วนตัว">
+        <Descriptions.Item label="ชื่อ-นามสกุล">
+          {`${authDetailSelector.get('firstName') ||
+            ''} ${authDetailSelector.get('lastName') ||
+            ''}`}
+        </Descriptions.Item>
+        <Descriptions.Item label="เบอร์โทรศัพท์">
+          {authDetailSelector.get('phoneNumber') || ''}
+        </Descriptions.Item>
+        <Descriptions.Item label="วันเกิด">
+          {authDetailSelector.get('birthDate') || ''}
+        </Descriptions.Item>
+        <Descriptions.Item label="เลขบัตรประจำตัวประชาชน">
+          {authDetailSelector.get('citizenId') || ''}
+        </Descriptions.Item>
+        <Descriptions.Item label="ที่อยู่">
+          {`บ้านเลขที่. ${addressJson.thaiAddressMoo ||
+            ''}, ${addressJson.thaiAddressThanon ||
+            ''}, ต. ${addressJson.thaiAddressDistrict ||
+            ''} อ. ${addressJson.thaiAddressAmphur ||
+            ''} จ. ${addressJson.thaiAddressProvince ||
+            ''} ${addressJson.zipCode || ''} `}
+        </Descriptions.Item>
+      </Descriptions>
+    </Card>
+  )
+}
 
 export default ProfileCard
