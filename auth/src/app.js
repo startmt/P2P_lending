622bea4798a3dafd1f2aa7b2ebb3a049de7b69f2
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-12-27 00:17:14 +0700

    [ADD] refactor

--- auth/src/app.js
+++ auth/src/app.js
@@ -2,7 +2,6 @@ import express from 'express'
 import http from 'http'
 import routes from './route/index'
 import bodyParser from 'body-parser'
-import connectMongo from './mongo'
 import { connectRedis } from './redis'
 import cors from 'cors'
 import { connectMysql } from './mysql'
@@ -12,7 +11,6 @@ const PORT = '3000'
 http.createServer(app).listen(PORT, async() => {
   
   await connectMysql()
-  connectMongo()
   connectRedis()
 })
 
