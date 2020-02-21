94ec2dc0f568abe19ef2de2243a373e4f49dfe75
Chansin Thongkum <59070038@it.kmitl.ac.th>
2020-02-21 15:55:41 +0700

    [ADD] upload file

--- auth/src/controller/lending/createLendingController.js
+++ auth/src/controller/lending/createLendingController.js
@@ -1,9 +1,9 @@
 import { status400, status200 } from '../../utils/status'
 import { createLending } from '../../service/lending/borrowerService'
+import { uploadFile } from '../../utils/managefile'
+
 export default async (req, res) => {
   try {
-    console.log(req.files)
-    // const nsbFile = req.files
     const query = await createLending(req.authInfo.username, {
       category: req.body.category,
       title: req.body.title,
@@ -13,11 +13,14 @@ export default async (req, res) => {
       loanTenor: req.body.loanTenor,
       description: req.body.description,
     })
-    if (query.status === 200) {
+    const prefix = `${req.authInfo.username}-${query.data.id}`
+    const upload = await uploadFile(req.files, prefix)
+    if (upload === 200) {
       status200(res, query)
     }
     status400(res, query.message)
   } catch (e) {
+    console.log(e)
     status400(res)
   }
 }
