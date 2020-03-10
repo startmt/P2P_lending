9cff05c1338eaa318d0492e222865e6742da1458
Chansin Thongkum <59070038@it.kmitl.ac.th>
2020-03-10 21:46:23 +0700

    test

--- auth/src/controller/transaction/PaymentController.js
+++ auth/src/controller/transaction/PaymentController.js
@@ -2,12 +2,12 @@ import { status400, status200 } from '../../utils/status'
 import createPaymentURL from '../../service/omise/createPaymentURL'
 export default async (req, res) => {
   try {
-    // const usename = req.authInfo.username
-    // const lendingId = req.body.lendingId
-    // const bankName = req.body.bankName
-    const bankName = 'internet_banking_bay'
-    const price = 100000
-    const url = await createPaymentURL(price, bankName)
+    const usename = req.authInfo.username
+    const requestId = req.body.requestId
+    const bankName = req.body.bankName
+    // const bankName = 'internet_banking_bay'
+    const amount = 10000000
+    const url = await createPaymentURL(amount, bankName)
     status200(res, { url: url.data.authorize_uri })
   } catch (e) {
     status400(res, 'Error to create')
