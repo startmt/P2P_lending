62f1fa409db2a269f6efa03f539d3dd34b1b68e4
Chansin Thongkum <59070038@it.kmitl.ac.th>
2020-03-25 19:34:57 +0700

    reject by admin

--- auth/src/service/lending/lendingService.js
+++ auth/src/service/lending/lendingService.js
@@ -3,14 +3,24 @@ import { getUserById } from '../../crud/user'
 import { createLog } from '../../crud/requestlog'
 
 export const changeRequestState = async ({ requestId, state, userId }) => {
-  switch (state) {
-    case 'APPROVE_INIT':
-      return await appoveInit(requestId, 'CHECKED', userId)
+  try {
+    switch (state) {
+      case 'APPROVE_INIT':
+        return await appoveInit(requestId, 'CHECKED', userId)
+      case 'REJECT':
+        return await rejectInit(requestId, userId)
+    }
+  } catch (error) {
+    console.log(error)
+    return { message: 'error' }
   }
 }
 
 const appoveInit = async (requestId, state, adminId) => {
-  if (await checkRepeatState(requestId, state)) {
+  if (
+    (await checkRepeatState(requestId, state)) &&
+    (await checkRepeatState(requestId, 'REJECT'))
+  ) {
     try {
       const admin = await getUserById(adminId)
       const requestData = {
@@ -28,6 +38,25 @@ const appoveInit = async (requestId, state, adminId) => {
     }
   } else {
     return { status: 400, message: 'This request has been done.' }
+  }
+}
+
+const rejectInit = async (requestId, adminId) => {
+  try {
+    if (await checkRepeatState(requestId, 'REJECT')) {
+      const admin = await getUserById(adminId)
+      await updateRequest(requestId, { state: 'REJECT' })
+      await createLog(
+        `change INIT to REJECED by ${admin.get().username}`,
+        requestId,
+      )
+      return { status: 200 }
+    } else {
+      return { status: 400, message: 'This request has been rejected.' }
+    }
+  } catch (error) {
+    console.log(error)
+    return { status: 400, message: 'Something went wrong.' }
   }
 }
 
