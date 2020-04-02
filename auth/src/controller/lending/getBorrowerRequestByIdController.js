47c3962f59cfcc167780f5ee82d6616112cccdb7
Chansin Thongkum <59070038@it.kmitl.ac.th>
2020-04-02 18:21:32 +0700

    add user

--- auth/src/controller/lending/getBorrowerRequestByIdController.js
+++ auth/src/controller/lending/getBorrowerRequestByIdController.js
@@ -1,11 +1,22 @@
 import { status400, status200 } from '../../utils/status'
 import { getBorrowerRequestById } from '../../service/lending/borrowerService'
+import { getUserIdOnefromRequestId } from '../../crud/requestuser'
+import { getUserById } from '../../crud/user'
+import { getInfomationByUsername } from '../../crud/information'
 export default async (req, res) => {
   try {
     const query = await getBorrowerRequestById(req.params.id)
-    if (query.status === 200) status200(res, query.data || {})
-    status400(res, query.message)
+    const userrequestQuery = await getUserIdOnefromRequestId(query.data.id)
+    const user = await getUserById(userrequestQuery.get().userId)
+    const information = await getInfomationByUsername(user.get().username)
+    const data = {
+      user: information.get(),
+    }
+    Object.assign(data, query.data)
+    if (query.status === 200) status200(res, data || {})
+    return status400(res, query.message)
   } catch (e) {
-    status400(res)
+    console.log(e)
+    return status400(res)
   }
 }
