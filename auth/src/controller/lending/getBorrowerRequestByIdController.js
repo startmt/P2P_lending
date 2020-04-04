8c27f1b9ebc85c8debe7287f9fc09059c091c1fb
Chansin Thongkum <59070038@it.kmitl.ac.th>
2020-04-04 15:19:03 +0700

    ADD Contract in id

--- auth/src/controller/lending/getBorrowerRequestByIdController.js
+++ auth/src/controller/lending/getBorrowerRequestByIdController.js
@@ -3,17 +3,23 @@ import { getBorrowerRequestById } from '../../service/lending/borrowerService'
 import { getUserIdOnefromRequestId } from '../../crud/requestuser'
 import { getUserById } from '../../crud/user'
 import { getInfomationByUsername } from '../../crud/information'
+import { getContractById } from '../../crud/contract'
 export default async (req, res) => {
   try {
     const query = await getBorrowerRequestById(req.params.id)
     const userrequestQuery = await getUserIdOnefromRequestId(query.data.id)
     const user = await getUserById(userrequestQuery.get().userId)
     const information = await getInfomationByUsername(user.get().username)
+    const contract = await getContractById(req.params.id)
     const data = {
       user: information.get(),
+      contract: {},
+    }
+    if (contract) {
+      Object.assign(data.contract, contract.get())
     }
     Object.assign(data, query.data)
-    if (query.status === 200) status200(res, data || {})
+    if (query.status === 200) return status200(res, data || {})
     return status400(res, query.message)
   } catch (e) {
     console.log(e)
